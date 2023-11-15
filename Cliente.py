05f832f2ded95930e279d196c041fcc77caafc3a
Usuario_Curso <correocurso@email.com>
2023-11-14 22:59:18 -0300

    Solucionado la no muestra del usuario encontrado

--- Cliente.py
+++ Cliente.py
@@ -2,10 +2,9 @@ from Usuario import Usuario
 
 
 class Cliente(Usuario):
-    def __init__(self, codCliente):
+    def __init__(self, nombre, apellido, correo, edad, ciudad, codCliente):
+        super().__init__(nombre,apellido,correo,edad,ciudad)
         self._codCliente = codCliente
-        
-
 
 
 
